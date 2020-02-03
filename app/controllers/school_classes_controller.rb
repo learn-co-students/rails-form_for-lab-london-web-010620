b23b86c9eb8f69016dd373c03f3ed8b9ece5d47d
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-03 17:13:30 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,14 @@
 class SchoolClassesController < ApplicationController
-    def index
-        @school_classes = SchoolClass.all
-    end
-    
-    def new
+   def new
         @school_class = SchoolClass.new
     end
 
     def create
+        @school_class = SchoolClass.new
+        @school_class.title = params[:school_class][:title]
+        @school_class.room_number = params[:school_class][:room_number]
+        @school_class.save
+        redirect_to school_class_path(@school_class)
     end
 
     def show
@@ -15,8 +16,14 @@ class SchoolClassesController < ApplicationController
     end
 
     def edit
+        @school_class = SchoolClass.find(params[:id])
     end
 
     def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.title = params[:school_class][:title]
+        @school_class.room_number = params[:school_class][:room_number]
+        @school_class.save
+        redirect_to school_class_path(@school_class)
     end
 end 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,7 @@
 class StudentsController < ApplicationController
-    def index
-        @students = Student.all
-    end
-    
-    def new
-        @student = Student.new
-    end
+   
 
-    def create
-        @student = Student.new(student_params(:first_name, :last_name))
-        @student.save
-        redirect_to student_path(@student)
-    end
+    
 
     def show
         @student = Student.find(params[:id])
@@ -23,13 +13,24 @@ class StudentsController < ApplicationController
 
     def update
         @student = Student.find(params[:id])
-        @student.update(student_params(:first_name, :last_name))
+        @student.last_name = params[:student][:last_name]
+        @student.first_name = params[:student][:first_name]
+        @student.save
         redirect_to student_path(@student)
     end
 
-    private
-
-    def student_params(*args)
-        params.require(:student).permit(*args)
+    def new
+        @student = Student.new
     end
+    
+    def create
+        @student = Student.new
+        @student.first_name = params[:student][:first_name]
+        @student.last_name = params[:student][:last_name]
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+
+
 end
